e6d3541e489b092924b4ac6de0271778a79d4abd
Murugan V <visumurugan@gmail.com>
2023-10-04 12:05:36 +0530

    added-animations
    added-animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import './Card.css'
 
 export default function Card({ response }) {
+
     const namesArray = [
         "Alice",
         "Bob",
@@ -49,16 +50,41 @@ export default function Card({ response }) {
         "Sophia",
         "Tyler",
         "Ursula",
-      ];
-            
+    ];
+
     function addContentEditable(value) {
-        document.querySelector(`.${value}`).contentEditable = true;
+        const dom = document.querySelector(`#${value}`);
+        dom.contentEditable = true;
+        dom.focus();
+        dom.style.transform = "scale(1.5)"
+        dom.style.transition = "transform 0.25s ease";
     }
+
+    function removeAll(value) {
+        const dom = document.querySelector(`#${value}`);
+        dom.style.transform = ''
+        dom.blur();
+    }
+
+    function maxSize(value) {
+        // const dom = document.querySelector(`#${value}`);
+        // dom.style.transform = "scale(3)"
+        // console.log(dom);
+    }
+
     return (
-        <section className='grid-cards'> 
+        <section className='grid-cards'>
             {namesArray && namesArray.map((value, index) => {
-                return <span className={value} key={index}
-                    onClick={() => addContentEditable(value)}>{value}</span>
+                return (
+                    <>
+                        <span id={value} className='card-class' key={index}
+                            onMouseOut={() => removeAll(value)} onClick={() => addContentEditable(value)}>{value}
+                            <svg style={{ marginTop: 'auto' }} xmlns="http://www.w3.org/2000/svg" onClick={() => maxSize(value)} viewBox="0 0 20 20" fill="currentColor" height={12} width={12}>
+                                <path d="M13.28 7.78l3.22-3.22v2.69a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.69l-3.22 3.22a.75.75 0 001.06 1.06zM2 17.25v-4.5a.75.75 0 011.5 0v2.69l3.22-3.22a.75.75 0 011.06 1.06L4.56 16.5h2.69a.75.75 0 010 1.5h-4.5a.747.747 0 01-.75-.75zM12.22 13.28l3.22 3.22h-2.69a.75.75 0 000 1.5h4.5a.747.747 0 00.75-.75v-4.5a.75.75 0 00-1.5 0v2.69l-3.22-3.22a.75.75 0 10-1.06 1.06zM3.5 4.56l3.22 3.22a.75.75 0 001.06-1.06L4.56 3.5h2.69a.75.75 0 000-1.5h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0V4.56z" />
+                            </svg>
+                        </span>
+                    </>
+                )
             })
             }
         </section>
